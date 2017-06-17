f35a592e0e8e5b71e5bcca9e8c017863d7986faf
haribalajip <hari111.balaji@gmail.com>
2017-06-17 22:31:33 +0530

    added controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,3 +50,35 @@ angular
       controller:"homeController"
       })
   });
+
+//services
+weatherApp.service('forecastService',function(){
+this.city="bangalore";
+})
+
+//controller
+weatherApp.controller('homeController',['$scope','forecastService',function($scope,forecastService){
+  $scope.city=forecastService.city;
+  $scope.$watch('city',function(){
+    forecastService.city=$scope.city;
+  });    
+}]);
+
+weatherApp.controller("forecastController",['$scope','$resource','$routeParams','forecastService',function($scope,$resource,$routeParams,forecastService){
+$scope.city=forecastService.city;
+this.appid="4fd4dd83b901576fbcde60961152fdd2";    
+$scope.days=$routeParams.num||2;
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };    
+    $scope.convertToCelsius=function(temp){
+        return Math.round(temp-273);
+    }
+$scope.weatherAPI=$resource("http://api.openweathermap.org/data/2.5/forecast/daily?");
+$scope.weatherResult=$scope.weatherAPI.get({q:$scope.city,cnt:$scope.days,appid:this.appid});
+    console.log($scope.weatherResult);
+}]);
+
