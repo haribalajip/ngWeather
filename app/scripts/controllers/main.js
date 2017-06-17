6943771a8b8fbf8534d4c7f8b5d5aae3f72261c9
haribalajip <hari111.balaji@gmail.com>
2017-06-17 22:15:08 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngWeatherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngWeatherApp
+ */
+angular.module('ngWeatherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
